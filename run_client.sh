455eafff5ac7def845b883043171173a2323d3c5
sngjuk <sngjuk@gmail.com>
2019-07-31 04:10:27 +0000

    minor fix

--- run_client.sh
+++ run_client.sh
@@ -4,5 +4,8 @@ python3 -m virtualenv fuzzy_env
 source fuzzy_env/bin/activate
 pip3 install -r client_requirements.txt
 chmod +x client.py
+
+echo "-=-=-=-= install done -=-=-=--\n\n"
+
 ./client.py
 
